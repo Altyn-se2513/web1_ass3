246ad992d1847b235a4983d5a0940d0962d5534a
Altyn-se2513 <250886@astanait.edu.kz>
2026-09-26 17:55:34 +0500

    Update and rename firebase.js to js/firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,3 @@
-/* =========================================================
-   Firebase Configuration
-   Author: Altyn Abdinurova | Group: SE-2513
-   ========================================================= */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
